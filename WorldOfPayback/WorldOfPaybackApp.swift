7c385317cae0130c686a88facd694e315fb3e117
Ali Yasin Eser <aliyasineser@gmail.com>
2023-05-04 23:20:01 +0200

    Initial Commit

---
+++ WorldOfPayback/WorldOfPaybackApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPaybackApp.swift
+//  WorldOfPayback
+//
+//  Created by Ali Yasin Eser on 04.05.23.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPaybackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
